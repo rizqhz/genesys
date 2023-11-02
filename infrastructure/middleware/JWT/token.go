2a45e4760f76561f1bb411dda29844a3037aef66
Rizqi Ghozali <rizqi.ghoz@gmail.com>
2023-11-02 12:08:19 +0700

    Edit durasi token

--- infrastructure/middleware/JWT/token.go
+++ infrastructure/middleware/JWT/token.go
@@ -27,11 +27,11 @@ func NewJwtToken(user, role string) *JwtToken {
 func (t *JwtToken) Generate() {
 	var sign *JwtSign
 	// Generate JWT Access Token
-	sign = NewJwtSign(t.claim, "HS256", time.Minute*5)
+	sign = NewJwtSign(t.claim, "HS256", time.Minute*30)
 	sign.Generate(t.key.AccessKey)
 	t.AccessToken = sign.Stringify()
 	// Generate JWT Refresh Token
-	sign = NewJwtSign(t.claim, "HS256", time.Minute*15)
+	sign = NewJwtSign(t.claim, "HS256", time.Minute*60)
 	sign.Generate(t.key.RefreshKey)
 	t.RefreshToken = sign.Stringify()
 }
