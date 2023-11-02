5d41da0dcfca45be48a36dfa2fb4f6b07f0308d8
Rizqi Ghozali <rizqi.ghoz@gmail.com>
2023-11-02 11:58:41 +0700

    Update

--- internal/route/api.go
+++ internal/route/api.go
@@ -4,8 +4,10 @@ import (
 	echo "github.com/labstack/echo/v4"
 	mid "github.com/rizghz/genesys/infrastructure/middleware"
 	jwt "github.com/rizghz/genesys/infrastructure/middleware/JWT"
+	asisten "github.com/rizghz/genesys/module/Asisten/handler"
 	auth "github.com/rizghz/genesys/module/Auth/handler"
 	kelas "github.com/rizghz/genesys/module/Kelas/handler"
+	mahasiswa "github.com/rizghz/genesys/module/Mahasiswa/handler"
 	matkum "github.com/rizghz/genesys/module/MataPraktikum/handler"
 	ruangan "github.com/rizghz/genesys/module/Ruangan/handler"
 )
@@ -27,13 +29,13 @@ func Auth(e *echo.Echo, handler auth.AuthHandler) {
 }
 
 func User(e *echo.Echo, handler auth.UserHandler) {
-	e.GET("/users", handler.Index())
-	e.GET("/users/:id", handler.Observe())
-	e.POST("/users", handler.Store())
-	e.PUT("/users/:id", handler.Edit())
-	e.DELETE("/users/:id", handler.Destroy())
-	e.GET("/users/creds", nil)
-	e.GET("/users/:id/creds", nil)
+	e.GET("/users", handler.Index(), jwt.Impl())
+	e.GET("/users/:id", handler.Observe(), jwt.Impl())
+	e.POST("/users", handler.Store(), jwt.Impl())
+	e.PUT("/users/:id", handler.Edit(), jwt.Impl())
+	e.DELETE("/users/:id", handler.Destroy(), jwt.Impl())
+	// e.GET("/users/creds", nil, jwt.Impl())
+	// e.GET("/users/:id/creds", nil, jwt.Impl())
 }
 
 func Kelas(e *echo.Echo, handler kelas.KelasHandler) {
@@ -45,17 +47,33 @@ func Kelas(e *echo.Echo, handler kelas.KelasHandler) {
 }
 
 func Ruangan(e *echo.Echo, handler ruangan.RuanganHandler) {
-	e.GET("/ruangan", handler.Index())
-	e.GET("/ruangan/:kode", handler.Observe())
-	e.POST("/ruangan", handler.Store())
-	e.PUT("/ruangan/:kode", handler.Edit())
-	e.DELETE("/ruangan/:kode", handler.Destroy())
+	e.GET("/ruangan", handler.Index(), jwt.Impl())
+	e.GET("/ruangan/:kode", handler.Observe(), jwt.Impl())
+	e.POST("/ruangan", handler.Store(), jwt.Impl())
+	e.PUT("/ruangan/:kode", handler.Edit(), jwt.Impl())
+	e.DELETE("/ruangan/:kode", handler.Destroy(), jwt.Impl())
 }
 
 func MataPraktikum(e *echo.Echo, handler matkum.MataPraktikumHandler) {
-	e.GET("/matapraktikum", handler.Index())
-	e.GET("/matapraktikum/:kode", handler.Observe())
-	e.POST("/matapraktikum", handler.Store())
-	e.PUT("/matapraktikum/:kode", handler.Edit())
-	e.DELETE("/matapraktikum/:kode", handler.Destroy())
+	e.GET("/matapraktikum", handler.Index(), jwt.Impl())
+	e.GET("/matapraktikum/:kode", handler.Observe(), jwt.Impl())
+	e.POST("/matapraktikum", handler.Store(), jwt.Impl())
+	e.PUT("/matapraktikum/:kode", handler.Edit(), jwt.Impl())
+	e.DELETE("/matapraktikum/:kode", handler.Destroy(), jwt.Impl())
+}
+
+func Mahasiswa(e *echo.Echo, handler mahasiswa.MahasiswaHandler) {
+	e.GET("/mahasiswa", handler.Index(), jwt.Impl())
+	e.GET("/mahasiswa/:npm", handler.Observe(), jwt.Impl())
+	e.POST("/mahasiswa", handler.Store(), jwt.Impl())
+	e.PUT("/mahasiswa/:npm", handler.Edit(), jwt.Impl())
+	e.DELETE("/mahasiswa/:npm", handler.Destroy(), jwt.Impl())
+}
+
+func Asisten(e *echo.Echo, handler asisten.AsistenHandler) {
+	e.GET("/asisten", handler.Index(), jwt.Impl())
+	e.GET("/asisten/:nias", handler.Observe(), jwt.Impl())
+	e.POST("/asisten", handler.Store(), jwt.Impl())
+	e.PUT("/asisten/:nias", handler.Edit(), jwt.Impl())
+	e.DELETE("/asisten/:nias", handler.Destroy(), jwt.Impl())
 }
--- main.go
+++ main.go
@@ -6,26 +6,32 @@ import (
 	"github.com/rizghz/genesys/infrastructure/service/cloudinary"
 	"github.com/rizghz/genesys/internal/database/migration"
 	"github.com/rizghz/genesys/internal/route"
-	asisten "github.com/rizghz/genesys/module/Asisten"
-	be "github.com/rizghz/genesys/module/Auth"
-	bh "github.com/rizghz/genesys/module/Auth/handler"
-	br "github.com/rizghz/genesys/module/Auth/repository"
-	bs "github.com/rizghz/genesys/module/Auth/service"
-	ke "github.com/rizghz/genesys/module/Kelas"
-	kh "github.com/rizghz/genesys/module/Kelas/handler"
-	kr "github.com/rizghz/genesys/module/Kelas/repository"
-	ks "github.com/rizghz/genesys/module/Kelas/service"
-	ae "github.com/rizghz/genesys/module/Mahasiswa"
-	me "github.com/rizghz/genesys/module/MataPraktikum"
-	mh "github.com/rizghz/genesys/module/MataPraktikum/handler"
-	mr "github.com/rizghz/genesys/module/MataPraktikum/repository"
-	ms "github.com/rizghz/genesys/module/MataPraktikum/service"
-	penjadwalan "github.com/rizghz/genesys/module/Penjadwalan"
-	praktikum "github.com/rizghz/genesys/module/Praktikum"
-	re "github.com/rizghz/genesys/module/Ruangan"
-	rh "github.com/rizghz/genesys/module/Ruangan/handler"
-	rr "github.com/rizghz/genesys/module/Ruangan/repository"
-	rs "github.com/rizghz/genesys/module/Ruangan/service"
+	. "github.com/rizghz/genesys/module/Asisten"
+	. "github.com/rizghz/genesys/module/Asisten/handler"
+	. "github.com/rizghz/genesys/module/Asisten/repository"
+	. "github.com/rizghz/genesys/module/Asisten/service"
+	. "github.com/rizghz/genesys/module/Auth"
+	. "github.com/rizghz/genesys/module/Auth/handler"
+	auth_r "github.com/rizghz/genesys/module/Auth/repository"
+	. "github.com/rizghz/genesys/module/Auth/service"
+	. "github.com/rizghz/genesys/module/Kelas"
+	. "github.com/rizghz/genesys/module/Kelas/handler"
+	kelas_r "github.com/rizghz/genesys/module/Kelas/repository"
+	. "github.com/rizghz/genesys/module/Kelas/service"
+	. "github.com/rizghz/genesys/module/Mahasiswa"
+	. "github.com/rizghz/genesys/module/Mahasiswa/handler"
+	mahasiswa_r "github.com/rizghz/genesys/module/Mahasiswa/repository"
+	. "github.com/rizghz/genesys/module/Mahasiswa/service"
+	. "github.com/rizghz/genesys/module/MataPraktikum"
+	. "github.com/rizghz/genesys/module/MataPraktikum/handler"
+	mata_r "github.com/rizghz/genesys/module/MataPraktikum/repository"
+	. "github.com/rizghz/genesys/module/MataPraktikum/service"
+	. "github.com/rizghz/genesys/module/Penjadwalan"
+	. "github.com/rizghz/genesys/module/Praktikum"
+	. "github.com/rizghz/genesys/module/Ruangan"
+	. "github.com/rizghz/genesys/module/Ruangan/handler"
+	ruang_r "github.com/rizghz/genesys/module/Ruangan/repository"
+	. "github.com/rizghz/genesys/module/Ruangan/service"
 )
 
 func main() {
@@ -33,13 +39,15 @@ func main() {
 	migrate(driver)
 
 	e := echo.New()
-
 	route.Middleware(e)
+
 	UserModule(e, driver)
 	AuthModule(e, driver)
 	KelasModule(e, driver)
 	RuanganModule(e, driver)
 	MataPraktikumModule(e, driver)
+	MahasiswaModule(e, driver)
+	AsistenModule(e, driver)
 
 	e.Logger.Fatal(e.Start(":8008"))
 }
@@ -47,60 +55,74 @@ func main() {
 func migrate(driver *mysql.MySqlDriver) {
 	migrator := migration.NewMySqlMigrator(driver)
 	migrator.DropTable([]migration.Table{
-		asisten.AsistenModel{},
-		penjadwalan.PenjadwalanModel{},
-		be.UserModel{},
-		be.CredentialModel{},
-		ke.KelasModel{},
-		ae.MahasiswaModel{},
-		re.RuanganModel{},
-		me.MataPraktikumModel{},
-		praktikum.PraktikumModel{},
+		UserModel{},
+		CredentialModel{},
+		KelasModel{},
+		MahasiswaModel{},
+		RuanganModel{},
+		AsistenModel{},
+		MataPraktikumModel{},
+		PenjadwalanModel{},
+		PraktikumModel{},
 	}...)
 	migrator.CreateTable([]migration.Table{
-		asisten.AsistenModel{},
-		penjadwalan.PenjadwalanModel{},
-		be.UserModel{},
-		be.CredentialModel{},
-		ke.KelasModel{},
-		ae.MahasiswaModel{},
-		re.RuanganModel{},
-		me.MataPraktikumModel{},
-		praktikum.PraktikumModel{},
+		UserModel{},
+		CredentialModel{},
+		KelasModel{},
+		MahasiswaModel{},
+		RuanganModel{},
+		AsistenModel{},
+		MataPraktikumModel{},
+		PenjadwalanModel{},
+		PraktikumModel{},
 	}...)
 }
 
 func UserModule(e *echo.Echo, driver *mysql.MySqlDriver) {
-	repository := br.NewUserMySqlRepository(driver)
-	service := bs.NewUserServiceImpl(repository, cloudinary.NewImageUploader())
-	handler := bh.NewUserHttpHandler(service)
+	repository := auth_r.NewUserMySqlRepository(driver)
+	service := NewUserServiceImpl(repository, cloudinary.NewImageUploader())
+	handler := NewUserHttpHandler(service)
 	route.User(e, handler)
 }
 
 func AuthModule(e *echo.Echo, driver *mysql.MySqlDriver) {
-	repository := br.NewCredentialMySqlRepository(driver)
-	service := bs.NewAuthServiceImpl(repository)
-	handler := bh.NewAuthHttpHandler(service)
+	repository := auth_r.NewCredentialMySqlRepository(driver)
+	service := NewAuthServiceImpl(repository)
+	handler := NewAuthHttpHandler(service)
 	route.Auth(e, handler)
 }
 
 func KelasModule(e *echo.Echo, driver *mysql.MySqlDriver) {
-	repository := kr.NewKelasMySqlRepository(driver)
-	service := ks.NewKelasServiceImpl(repository)
-	handler := kh.NewKelasHttpHandler(service)
+	repository := kelas_r.NewKelasMySqlRepository(driver)
+	service := NewKelasServiceImpl(repository)
+	handler := NewKelasHttpHandler(service)
 	route.Kelas(e, handler)
 }
 
 func RuanganModule(e *echo.Echo, driver *mysql.MySqlDriver) {
-	repository := rr.NewRuanganMySqlRepository(driver)
-	service := rs.NewRuanganServiceImpl(repository)
-	handler := rh.NewRuanganHttpHandler(service)
+	repository := ruang_r.NewRuanganMySqlRepository(driver)
+	service := NewRuanganServiceImpl(repository)
+	handler := NewRuanganHttpHandler(service)
 	route.Ruangan(e, handler)
 }
 
 func MataPraktikumModule(e *echo.Echo, driver *mysql.MySqlDriver) {
-	repository := mr.NewMataPraktikumMySqlRepository(driver)
-	service := ms.NewMataPraktikumServiceImpl(repository)
-	handler := mh.NewMataPraktikumHttpHandler(service)
+	repository := mata_r.NewMataPraktikumMySqlRepository(driver)
+	service := NewMataPraktikumServiceImpl(repository)
+	handler := NewMataPraktikumHttpHandler(service)
 	route.MataPraktikum(e, handler)
+}
+
+func MahasiswaModule(e *echo.Echo, driver *mysql.MySqlDriver) {
+	repository := mahasiswa_r.NewMahasiswaMySqlRepository(driver)
+	service := NewMahasiswaServiceImpl(repository)
+	handler := NewMahasiswaHttpHandler(service)
+	route.Mahasiswa(e, handler)
+}
+
+func AsistenModule(e *echo.Echo, driver *mysql.MySqlDriver) {
+	repository := NewAsistenMySqlRepository(driver)
+	service := NewAsistenServiceImpl(repository)
+	handler := NewAsistenHttpHandler(service)
+	route.Asisten(e, handler)
 }
